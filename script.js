efd9204d930960a3422f1ef0e2462584cae8a60c
kz-git819 <callchan2525@gmail.com>
2025-05-16 20:16:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Script loaded");
+// ここにボタンの動作を追加予定
+document.getElementById("hideAll").addEventListener("click", () => {
+  alert("すべて隠す");
+});
+document.getElementById("showAll").addEventListener("click", () => {
+  alert("すべて表示");
+});
+document.getElementById("toggleLock").addEventListener("click", () => {
+  alert("ロック切り替え");
+});
